7ac312dd79703af2121948decd163ef90fb14d3d
wangzh <alanzw8@gmail.com>
2025-03-25 22:00:41 +0800

    fix: trans2pc real

--- misc/trans2pc.py
+++ misc/trans2pc.py
@@ -6,7 +6,7 @@ import scipy.io as sio
 import numpy as np
 import open3d as o3d
 
-mi.set_variant('cuda_ad_rgb')
+mi.set_variant('llvm_ad_rgb')
 import mitransient as mitr
 from tqdm import tqdm
 
@@ -48,6 +48,7 @@ def multi_view_generate_point_cloud_synthetic(scene_path : str,
     fov_rad = np.deg2rad(fov)  
     num_views = len(origin_list)
     point_list = []
+    color_list = []
 
     for i in tqdm(range(num_views), desc="Generating point cloud"):
 
@@ -59,9 +60,9 @@ def multi_view_generate_point_cloud_synthetic(scene_path : str,
         params['PointLight.position'] = origin
         params.update()
         _, data_transient = mi.render(scene, spp=spp)
-        data_transient_np = np.sum(np.array(data_transient), axis=-1)
         
-        depth_map = np.argmax(data_transient_np, axis=2) * bin_width_opl
+        depth_map = np.argmax(np.sum(np.array(data_transient), axis=-1), axis=2) * bin_width_opl
+        color_map = np.max(np.array(data_transient), axis=2) / ((depth_map[..., np.newaxis] / (depth_map.max() + 1e-5)) ** 2 + 1e-5)
             
         forward = target - origin
         forward = forward / np.linalg.norm(forward) 
@@ -84,15 +85,20 @@ def multi_view_generate_point_cloud_synthetic(scene_path : str,
         )
         directions = directions / np.linalg.norm(directions, axis=2, keepdims=True) 
 
-        point_cloud = origin + directions * depth_map[:, :, np.newaxis]
+        point_cloud = origin + directions * depth_map[:, :, np.newaxis] 
 
         point_cloud = point_cloud.reshape(-1, 3)
+        color_map = color_map.reshape(-1, 3)
 
         point_list.append(point_cloud)
+        color_list.append(color_map)
 
     point_list = np.concatenate(point_list, axis=0)
     point_cloud = o3d.geometry.PointCloud()
     point_cloud.points = o3d.utility.Vector3dVector(point_list)
+    color_list = np.concatenate(color_list, axis=0)
+
+    point_cloud.colors = o3d.utility.Vector3dVector(color_list)
     if down_rate < 1.0:
         point_cloud = point_cloud.random_down_sample(sampling_ratio=down_rate)
     o3d.io.write_point_cloud(point_cloud_path, point_cloud)
@@ -125,44 +131,47 @@ def multi_view_generate_point_cloud_real(transient_dir: str,
 
     
     width, height = resolution
+    fov = 24
     fov_rad = np.deg2rad(fov)  
     transient_list = sorted(os.listdir(transient_dir))
     num_views = len(transient_list)
     point_list = []
 
     for i in tqdm(range(num_views), desc="Generating point cloud"):
-        
-
-
+        # if i >= 1:
+        #     continue
+        # if not (transient_list[i].endswith('_0.mat') or transient_list[i].endswith('_180.mat')):
+        #     continue
         data = sio.loadmat(os.path.join(transient_dir, transient_list[i]))
         origin = data['camera_coordinate'][:, 0]
-        origin = origin * 1.1
+        # import pdb; pdb.set_trace()
+        # origin = origin * 1.1
         target = data['target_vector'][:, 0]
         up = data['up_vector'][:, 0]
+        # data_transient = np.roll(data['shifted_data'], shift=-230, axis=1).reshape(width, height, -1)
         data_transient = data['shifted_data'].reshape(width, height, -1)
-        data_transient[..., 0:100] = 0
-        data_transient[..., -100:] = 0
-        fov = 24
-
+        
 
         # data_transient = load(os.path.join(transient_dir, f"{i:02d}"))
-        
+
+        # import pdb; pdb.set_trace()
         depth_map = np.argmax(data_transient, axis=2) / 2 * bin_width_opl
         depth_map = depth_map.T # MATLAB convert to numpy
-        # import pdb; pdb.set_trace()
-        forward = target - origin
-        forward = forward / np.linalg.norm(forward) 
+        depth_map = np.flip(depth_map, axis=1)
+        # depth_map[...] = -origin[2] * 0.91
+        forward = target
+        forward = target / np.linalg.norm(target) 
         # import pdb; pdb.set_trace()
         right = np.cross(forward, up)
         right = right / np.linalg.norm(right)  
         up = np.cross(right, forward) 
 
         aspect_ratio = width / height
-        plane_height = 2 * np.tan(fov_rad / 2)  
+        plane_height = 2 * np.tan(fov_rad / 2)
         plane_width = plane_height * aspect_ratio 
 
         x = np.linspace(-plane_width / 2, plane_width / 2, width)
-        y = np.linspace(plane_height / 3 * 2, -plane_height / 3, height)
+        y = np.linspace(plane_height / 2, -plane_height / 2, height)
         xx, yy = np.meshgrid(x, y)
 
         directions = (
@@ -170,22 +179,27 @@ def multi_view_generate_point_cloud_real(transient_dir: str,
             right[np.newaxis, np.newaxis, :] * xx[:, :, np.newaxis] +
             up[np.newaxis, np.newaxis, :] * yy[:, :, np.newaxis]
         )
-        directions = directions / np.linalg.norm(directions, axis=2, keepdims=True) 
-
+        # import pdb; pdb.set_trace()
+        directions = directions / np.linalg.norm(directions, axis=-1, keepdims=True) 
+        # directions[...] = np.array([0, 0, 1])
         point_cloud = origin + directions * depth_map[:, :, np.newaxis]
 
         point_cloud = point_cloud.reshape(-1, 3)
+        for j in range(point_cloud.shape[0]):
+            if np.abs(point_cloud[j, 0]) > 0.2 or np.abs(point_cloud[j, 2]) > 0.2:
+                point_cloud[j, :] = np.array([0, 0, 0])
 
         point_list.append(point_cloud)
 
     point_list = np.concatenate(point_list, axis=0)
+    # import pdb; pdb.settrace()_
     point_cloud = o3d.geometry.PointCloud()
     point_cloud.points = o3d.utility.Vector3dVector(point_list)
     if down_rate < 1.0:
         point_cloud = point_cloud.random_down_sample(sampling_ratio=down_rate)
     o3d.io.write_point_cloud(point_cloud_path, point_cloud)
 if __name__ == '__main__':
-    # scene_path = os.path.abspath('/public/home/wangzh1/iccp2025/cornell-box/cbox_diffuse.xml')
+    scene_path = os.path.abspath('cornell-box/cbox_diffuse.xml')
     # # scene = mi.load_file(os.path.abspath('cornell-box/cbox_diffuse.xml'))
     # multi_view_generate_point_cloud_synthetic(scene_path=scene_path,
     #                                             origin_list=np.array([[278, 278, -800], [-800, 278, 278], [278, 278, 1200], [1200, 278, 278]]),
@@ -193,12 +207,12 @@ if __name__ == '__main__':
     #                                             up=np.array([0, 1, 0]),
     #                                             fov=50,
     #                                             resolution=(400, 400),
-    #                                             spp=512,
+    #                                             spp=2048,
     #                                             down_rate=0.1,
-    #                                             point_cloud_path="pc.ply")
-    multi_view_generate_point_cloud_real(transient_dir='/public/home/wangzh1/iccp2025/mitsuba3-workflow/transient_scenes/real/dragon-3-17',
+    #                                             point_cloud_path="cornell.ply")
+    multi_view_generate_point_cloud_real(transient_dir='/Users/wangzh/Desktop/ShanghaiTech/Spring_2025/ICCP2025/mitsuba-tutorials/mitransient-1.1.0/examples/transient/hulu',
                                          fov=24,
                                          resolution=(128, 128),
-                                         bin_width_opl=0.0012, # 4ps = 0.0012m
+                                         bin_width_opl=0.0012 * 8, # 4ps = 0.0012m
                                          down_rate=1,
-                                         point_cloud_path='dragon.ply')
+                                         point_cloud_path='hulu.ply')
